303522bdafdcad2b436937a7ae1102bd67c851cd
fconsoni <franco.consoni@gmail.com>
2021-11-23 07:47:57 -0300

    Updated Arch

--- Clean Swift/Router.xctemplate/___FILEBASENAME___Router.swift
+++ Clean Swift/Router.xctemplate/___FILEBASENAME___Router.swift
@@ -1,10 +1,3 @@
-//
-//  ___FILENAME___
-//  ___PROJECTNAME___
-//
-//  Created by ___FULLUSERNAME___ on ___DATE___.
-//  Copyright (c) ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
-
 import UIKit
 
 protocol ___VARIABLE_sceneName___Routable {
@@ -12,7 +5,7 @@ protocol ___VARIABLE_sceneName___Routable {
 }
 
 final class ___VARIABLE_sceneName___Router: ___VARIABLE_sceneName___Routable {
-    weak var viewController: ___VARIABLE_sceneName___Showable?
+    weak var viewController: ___VARIABLE_sceneName___ViewController?
     
 	func routeToSomewhere() {
         
--- Clean Swift/View Controller.xctemplate/___FILEBASENAME___ViewController.swift
+++ Clean Swift/View Controller.xctemplate/___FILEBASENAME___ViewController.swift
@@ -1,31 +1,15 @@
-//
-//  ___FILENAME___
-//  ___PROJECTNAME___
-//
-//  Created by ___FULLUSERNAME___ on ___DATE___.
-//  Copyright (c) ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
-
 import UIKit
 
-protocol ___VARIABLE_sceneName___Showable: class {
+protocol ___VARIABLE_sceneName___Showable: AnyObject {
     func displaySomething()
 }
 
 final class ___VARIABLE_sceneName___ViewController: UIViewController, ___VARIABLE_sceneName___Showable {
     var router: ___VARIABLE_sceneName___Routable?
-    var interactor:  ___VARIABLE_sceneName___Interactable?
+    var interactor: ___VARIABLE_sceneName___Interactable?
     
-    init() {
-        super.init(nibName: String(describing: ___VARIABLE_sceneName___ViewController.self), bundle: .none)
-    }
+    override func loadView() {
     
-    // MARK:- Object lifecycle
-    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
-        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
-    }
-    
-    required init?(coder aDecoder: NSCoder) {
-        super.init(nibName: String(describing: ___VARIABLE_sceneName___ViewController.self), bundle: .none)
     }
     
     override func viewDidLoad(){
